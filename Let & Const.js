00a514ffcdbb081a79cbef69db6cccbf6eb74fcc
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-13 20:21:37 +0600

    HackerRank Javascript Day1 Solution

---
+++ Let & Const.js
@@ -0,0 +1,16 @@
+function main() {
+  const r = readLine();
+  const PI = Math.PI;
+  console.log(PI * r * r);
+  console.log(2 * PI * r);
+}
+
+//   try {
+//     // Attempt to redefine the value of constant variable PI
+//     PI = 0;
+//     // Attempt to print the value of PI
+//     console.log(PI);
+//   } catch (error) {
+//     console.error("You correctly declared 'PI' as a constant.");
+//   }
+// }
