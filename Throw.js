dfd121905edfc3602f2e7cb228a85d0dabc1bbb1
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-09-15 21:14:36 +0600

    HackerRank Day3 Solved

---
+++ Throw.js
@@ -0,0 +1,50 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function isPositive(a) {
+  if (a > 0) {
+    return "YES";
+  } else if (a === 0) {
+    throw Error("Zero Error");
+  } else {
+    throw Error("Negative Error");
+  }
+}
+
+function main() {
+  const n = +readLine();
+
+  for (let i = 0; i < n; i++) {
+    const a = +readLine();
+
+    try {
+      console.log(isPositive(a));
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
